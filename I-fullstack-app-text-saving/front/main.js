315f6b2391d3b7937f5ec9955e82e3760fcc9f55
ShermanPI <shermanperez0@gmail.com>
2023-06-08 23:11:10 -0400

    messages rendering

--- I-fullstack-app-text-saving/front/main.js
+++ I-fullstack-app-text-saving/front/main.js
@@ -1,16 +1,38 @@
 let form = document.getElementById('hola-form')
-console.log("eo")
+
+
+const getMessages = ()=>{
+  fetch('http://localhost:5000/', {headers: {'Content-Type': 'text/plain'}})
+  .then(res=> res.ok ? res.text() : new Promise.reject())
+  .then(res => {
+    const messageArrays = res.split('\n'),
+      fragment = document.createDocumentFragment(),
+      list = document.querySelector('.messages-list')
+    
+    messageArrays.pop() //removing the last \n
+
+    messageArrays.forEach((message) => {
+      const li = document.createElement('li')
+      li.innerHTML = message 
+      fragment.appendChild(li)
+    });
+
+    list.appendChild(fragment)
+  })
+  .catch(err=>console.error(err))
+}
+
+getMessages()
+
 document.addEventListener('submit', (e)=>{
   e.preventDefault()
   
   if(e.target == form){
     fetch('http://localhost:5000/', 
     {method: 'POST',
-    body: form.info.value,
+    body: form.info.value.toString(),
     headers: {
       'Content-Type': 'text/plain'}
     })
-    .then((res)=> res.ok? console.log("SE ENVIO"): console.log("no se envio ono"))
-    console.log(form.info.value)
   }
 })
