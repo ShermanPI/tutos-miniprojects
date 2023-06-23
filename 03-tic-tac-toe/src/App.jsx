5bdba97322a9deda18aae95f980b619f57dafd31
Sherman PI <shermanperez0@gmail.com>
2023-06-23 15:34:09 -0400

    game done before tutorial

--- 03-tic-tac-toe/src/App.jsx
+++ 03-tic-tac-toe/src/App.jsx
@@ -1,23 +1,8 @@
-import Square from './Square'
+import Board from './Board'
 import './App.css'
 
 export default function App(){
-  let squares = []
-
-  for(let i = 0; i <= 8; i++){
-    console.log("hey")
-    squares.push(<Square  key={`square-${i}`}/>)
-  }
-  
-  //text="☀️🌑"
-  console.log(squares)
-
-  return(
-    <div className='game-container'>      
-      <h1>TIC TAC TOE</h1>
-      <div className="game-grid">
-          {squares}
-      </div>
-    </div>
+  return (
+    <Board />
   )
 }
