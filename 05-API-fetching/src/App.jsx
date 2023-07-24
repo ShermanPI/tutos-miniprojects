67b2d6df7f341393493f15914435537413928cda
Sherman PI <shermanperez0@gmail.com>
2023-07-24 14:41:47 -0400

    Trainee readme completed before tutorial

--- 05-API-fetching/src/App.jsx
+++ 05-API-fetching/src/App.jsx
@@ -1,3 +1,30 @@
-export default function App(){
-    return <h1>Holahey</h1>
+import { useEffect, useState } from 'react'
+import '../stylee.css'
+
+export default function App () {
+  const [fact, setFact] = useState('Lorem Ipsum')
+  const [catImageUrl, setCatImageUrl] = useState('static/img/360_F_266724172_Iy8gdKgMa7XmrhYYxLCxyhx6J7070Pr8.jpg')
+
+  const imagesApi = 'https://cataas.com'
+  const factsApi = 'https://catfact.ninja'
+
+  useEffect(() => {
+    fetch(`${factsApi}/fact`)
+      .then(res => res.json())
+      .then(data => setFact(data.fact))
+  }, [])
+
+  useEffect(() => {
+    fetch(`${imagesApi}/cat/says/${fact.split(' ')[0]}?json=true`)
+      .then(res => res.json())
+      .then(data => setCatImageUrl(`${imagesApi}${data.url}`))
+  }, [fact])
+
+  return (
+    <main>
+      <h1>Random Cats</h1>
+      <h2>{fact}</h2>
+      <img src={catImageUrl} alt='' />
+    </main>
+  )
 }
