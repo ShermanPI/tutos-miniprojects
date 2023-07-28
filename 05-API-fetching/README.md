696525b449c1e13ab191d403c96f0af61539bb1e
ShermanPI <shermanperez0@gmail.com>
2023-07-28 16:10:05 -0400

    Facts logic extracted from component

--- 05-API-fetching/README.md
+++ 05-API-fetching/README.md
@@ -7,6 +7,6 @@ Imagen random: https://cataas.com/cat/says/hello
 
 Recupera un hecho aleatorio de gatos de la primera API
 
-Recuperar la primera palabra del hecho
+Recuperar las tres primeras palabra del hecho
 
 Muestra una imagen de un gato con la primera palabra.
--- 05-API-fetching/src/App.jsx
+++ 05-API-fetching/src/App.jsx
@@ -5,7 +5,7 @@ export default function App () {
   const [fact, setFact] = useState()
   const [catImageUrl, setCatImageUrl] = useState()
 
-  const imagesApi = 'https://cataas.com'
+  const IMAGES_API = 'https://cataas.com'
   const factsApi = 'https://catfact.ninja'
 
   useEffect(() => {
@@ -15,18 +15,21 @@ export default function App () {
   }, [])
 
   useEffect(() => {
-    if(fact){
-      fetch(`${imagesApi}/cat/says/${fact.split(' ')[0]}?json=true`)
-        .then(res => res.json())
-        .then(data => setCatImageUrl(`${imagesApi}${data.url}`))
-    }
+    if(!fact) return
+    
+    const threeFirstWords = fact.split(' ').slice(0,3).join(' ')
+    
+    fetch(`${IMAGES_API}/cat/says/${threeFirstWords}?json=true`)
+      .then(res => res.json())
+      .then(data => setCatImageUrl(`${IMAGES_API}${data.url}`))
+
   }, [fact])
 
   return (
     <main>
       <h1>Random Cats</h1>
       <h2>{fact ?? 'Loading...'}</h2>
-      <img src={catImageUrl ?? 'static/img/360_F_266724172_Iy8gdKgMa7XmrhYYxLCxyhx6J7070Pr8.jpg'} alt='cat image' />
+      <img src={catImageUrl ?? 'static/img/360_F_266724172_Iy8gdKgMa7XmrhYYxLCxyhx6J7070Pr8.jpg'} alt={`image of cat extracted from ${fact}`} />
     </main>
   )
 }
