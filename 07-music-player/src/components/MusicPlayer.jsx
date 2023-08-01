3f44f13c22bc654630d59d1b5d0aad0a4c366ac5
Sherman PI <shermanperez0@gmail.com>
2023-08-01 16:25:23 -0400

    07 - first approach to play music, using new Audio

--- 07-music-player/src/components/MusicPlayer.jsx
+++ 07-music-player/src/components/MusicPlayer.jsx
@@ -1,33 +1,27 @@
-// import { useState } from 'react'
-
 import { usePlayer } from '../hooks/usePLayer'
 import { ControlBtn } from './ControlButton'
 
-const dummyMusic = [
-  { id: 'M-1111', name: 'Progreso', artists: ['Eladio Carrion'], url: undefined, musicImg: undefined },
-  { id: 'M-2222', name: 'Coco Chanel', artists: ['Eladio Carrion', 'Bad Bunny'], url: undefined, musicImg: undefined },
-  { id: 'M-3333', name: '3 Am', artists: ['Eladio Carrion'], url: undefined, musicImg: undefined },
-  { id: 'M-4444', name: 'Kemba Walker', artists: ['Eladio Carrion', 'Bad Bunny'], url: undefined, musicImg: undefined }
-]
-
 export const MusicPlayer = () => {
-  const { musicId, playNextSong, playPreviousSong } = usePlayer({ initialMusicId: 0 })
-  const actualMusic = dummyMusic[musicId]
-  const artists = actualMusic.artists
+  const { music, isPlaying, playPreviousSong, playNextSong, playSongHandler } = usePlayer({ initialMusicId: 0 })
+  const artists = music.artists
 
   return (
     <section className='music-player'>
       <h1>Music Player</h1>
-
-      {actualMusic ? <p className='music-name'>{dummyMusic[musicId].name}</p> : null}
-      {actualMusic
-        ? <p className='artist-names'>{artists.length > 1 ? artists.map((el, i) => i === (artists.length - 1) ? el : `${el}, `) : artists}</p>
+      {music
+        ? 
+        <>
+          <audio src={music.url} id='audio-tag' preload='auto'/>
+          <p className='artist-names'>{artists.length > 1 ? artists.map((el, i) => i === (artists.length - 1) ? el : `${el}, `) : artists}</p>
+          <p className='music-name'>{music.name}</p>
+        </>
         : null}
+
       <input type='range' min='0' max='100' step='5' defaultValue='20' />
 
       <section className='control-panel'>
         <ControlBtn buttonContent='⏮️' handleClick={playPreviousSong} />
-        <ControlBtn buttonContent='▶️' />
+        <ControlBtn buttonContent={isPlaying ? '⏸️' : '▶️'} handleClick={playSongHandler} />
         <ControlBtn buttonContent='⏭️' handleClick={playNextSong} />
         <ControlBtn buttonContent='🔁' />
       </section>
--- 07-music-player/src/hooks/usePlayer.js
+++ 07-music-player/src/hooks/usePlayer.js
@@ -1,19 +1,61 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+const dummyMusic = [
+  { id: 'M-1111', name: 'Progreso', artists: ['Eladio Carrion'], url:`../../static/music/Progreso.mp3`, musicImg: undefined },
+  { id: 'M-2222', name: 'Coco Chanel', artists: ['Eladio Carrion', 'Bad Bunny'], url:`../../static/music/Coco Chanel.mp3`, musicImg: undefined },
+  { id: 'M-3333', name: '3 Am', artists: ['Eladio Carrion'], url:`../../static/music/3 Am.mp3`, musicImg: undefined },
+  { id: 'M-4444', name: 'Kemba Walker', artists: ['Eladio Carrion', 'Bad Bunny', 'Shermanius'], url:`../../static/music/Kemba Walker.mp3`, musicImg: undefined }
+]
+
+const maxId = (dummyMusic.length - 1)
 
 export const usePlayer = ({ initialMusicId }) => {
   const [musicId, setMusicId] = useState(initialMusicId)
+  const [isPlaying, setIsPlaying ] = useState(false)
+
+  const music = dummyMusic[musicId]
+
+  useEffect(()=>{
+    const audio = new Audio(music.url)
+
+    if(!isPlaying){
+      audio.play()
+    }else{
+      audio.pause()
+    }
+    
+  }, [isPlaying])
 
   const playNextSong = () => {
-    setMusicId(musicId + 1)
+    if(musicId + 1 <= maxId){
+      setMusicId(musicId + 1)
+    }else{
+      setMusicId(0)
+    }
+
   }
 
   const playPreviousSong = () => {
-    setMusicId(musicId - 1)
+    if(musicId - 1 < 0){
+      setMusicId(maxId)
+    }else{
+      setMusicId(musicId - 1)
+    }
+  }
+
+  const playSongHandler = ()=>{
+    if(!isPlaying){
+      setIsPlaying(true)
+    }else{
+      setIsPlaying(false)
+    }
   }
 
   return {
-    musicId,
+    music,
+    isPlaying,
     playNextSong,
-    playPreviousSong
+    playPreviousSong,
+    playSongHandler
   }
 }
